eda140d6436e1f5037e7f5425d501b7e30f3b002
Vanessa <vanessarcoin@gmail.com>
2024-01-09 19:56:50 -0300

    Check logic

--- src/application/UI.java
+++ src/application/UI.java
@@ -57,8 +57,11 @@ public class UI {
 		System.out.println();
 		printCapturedPieces(captured);
 		System.out.println();
-		System.out.println("Turn :" + chessMatch.getTurn());
+		System.out.println("Turn : " + chessMatch.getTurn());
 		System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+		if (chessMatch.getCheck()) {
+			System.out.println("CHECK!");
+		}
 	}
 	
 	public static void printBoard(ChessPiece[][] pieces) {
@@ -107,11 +110,11 @@ public class UI {
 		System.out.println("Captured pieces:");
 		System.out.print("White: ");
 		System.out.print(ANSI_WHITE);
-		System.out.print(Arrays.toString(white.toArray()));
-		System.out.println(ANSI_RESET);
+		System.out.println(Arrays.toString(white.toArray()));
+		System.out.print(ANSI_RESET);
 		System.out.print("Black: ");
 		System.out.print(ANSI_YELLOW);
-		System.out.print(Arrays.toString(black.toArray()));
-		System.out.println(ANSI_RESET);
+		System.out.println(Arrays.toString(black.toArray()));
+		System.out.print(ANSI_RESET);
 	}
 }
